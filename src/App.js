f3628935471086fd097d778b83a8caff432f95d5
Sogol <108494457+Sogolsa@users.noreply.github.com>
2024-03-07 19:57:02 -0600

    code optimization for better performance

--- src/App.js
+++ src/App.js
@@ -16,18 +16,33 @@ const App = () => {
   const [currentCity, setCurrentCity] = useState('See all cities');
   const [loading, setLoading] = useState(true);
 
+  // For optimization of loading time when the app is initially opened.
+  const fetchInitialData = async () => {
+    // setLoading(true);
+    const allEvents = await getEvents();
+    setAllLocations(extractLocations(allEvents));
+    setEvents(allEvents);
+    // setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchInitialData();
+  }, []);
+
   // Populate the events state with events list from fetch
   const fetchData = async () => {
     setLoading(false);
+    // await new Promise((resolve) => setTimeout(resolve, 2000));
     const allEvents = await getEvents();
     const filteredEvents =
       currentCity === 'See all cities'
         ? allEvents
         : allEvents.filter((event) => event.location === currentCity);
     // Extract elements from index 0 up to currentNOE which is 32 (first 32 events)
-    setEvents(filteredEvents.slice(0, currentNOE));
+    if (filteredEvents !== undefined) {
+      setEvents(filteredEvents.slice(0, currentNOE));
+    }
     setAllLocations(extractLocations(allEvents));
-    // setLoading(false);
   };
 
   // Populate the list as soon as the App Component is mounted.
